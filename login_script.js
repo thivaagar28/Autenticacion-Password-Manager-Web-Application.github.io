200fa5c9d9a7bb21ffc5ad1f9d7168a3f3b0d1b3
thivaagar28 <94023077+thivaagar28@users.noreply.github.com>
2023-01-24 03:46:12 +0800

    Add files via upload

--- login_script.js
+++ login_script.js
@@ -1,6 +1,7 @@
+// Import necessary function from firebase nodes
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, set, onValue, get, push, child } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-
+import { getDatabase, ref, set, get, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+//variable declaration
 let pd, domain, user_name, master_password;
 
 //web app's Firebase configuration
@@ -16,32 +17,35 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Initialize Firebase database
 const database = getDatabase(app);
-
+//Document Onload execution
 document.addEventListener("DOMContentLoaded", ()=>{
     //get url of current page with domain and generated password paameters
     const queryString = window.location.search;
-    console.log(queryString);
+    //console.log(queryString);
     //extract parameters list
     const urlParams = new URLSearchParams(queryString);
 
     //extract specific paramaters
     domain = urlParams.get('domain');
     pd = urlParams.get('password');
-    console.log(pd);
-    console.log(domain);
+    //console.log(pd);
+    //console.log(domain);
 })
-
+//Login button execution
 document.getElementById("login").addEventListener("click", (e)=>{
-    console.log("Login clicked");
+    //prevent default action
     e.preventDefault();
     user_name = document.getElementById("username").value;
     master_password = document.getElementById("master_password").value;
     let messages = [];
+    //username input filed validation
     if(user_name == "" ){
         messages.push("Username is required");
         //console.log("Username is required");
     }
+    //master password input field validation
     if(master_password == "" ){
         messages.push("Password is required")
         //console.log("password is required");
@@ -50,22 +54,26 @@ document.getElementById("login").addEventListener("click", (e)=>{
         alert(messages.join("\n"));
     }
     if(user_name != "" && master_password != ""){
+        //set user profile location
         const target = ref(database, 'Users/' + user_name);
         let data;
+        //get user profile info
         get(target).then((snapshot) => {
+            //if user available
             if (snapshot.exists()) {
                 data = snapshot.val();
-                console.log(data);
+                //console.log(data);
+                //Check password compatability
                 if (data.Masterpassword === master_password) {
                     alert("Successfull Login");
-                    console.log("account_found");   
+                    //console.log("account_found");   
                     account_found();
                 }
                 else{
                     alert('password does not match');
                 }
             } else {
-                console.log("No data available");
+                //console.log("No data available");
                 alert('user does not exist');
             }
         }).catch((error) => {
@@ -73,26 +81,31 @@ document.getElementById("login").addEventListener("click", (e)=>{
         });
     }
 })
-
 function account_found(){
+    //hide login div
     document.getElementById("login_feature").style.display = "none";
+    //display password div
     document.getElementById("password_manager").style.display = "block";
+    //Fill the input field with values of url parameters
     document.getElementById("pl_dm").value = domain;
     document.getElementById("password").value = pd;
     password_display();
 }
-
 function password_display(){
-    console.log("Password display function");
+    //console.log("Password display function");
     document.getElementById("login").style.display = "none";
+    //set target to user's available platforms
     const target = ref(database, 'Users/' + user_name + '/' + "Platform/");
     let uid, data, keys;
     let isfound = false;
+    //get all the available platforms unique ids
     get(target).then((snapshot) => {
+        //if any plaforms exist
         if (snapshot.exists()) {
-            console.log(snapshot.val());
+            //console.log(snapshot.val());
             data = snapshot.val();
             keys = Object.keys(data);
+            //traverse trough each unique ids child
             for(var i=0; i<keys.length; i++){
                 const k = keys[i];
                 if(data[k].platform == domain){
@@ -101,8 +114,10 @@ function password_display(){
                     break;
                 }
             }
+            //PLatform found
             if(isfound==true){
-                console.log("platform found");
+                //console.log("platform found");
+                //Fill with user's platform identifier and password
                 document.getElementById("identifier").value = data[uid].id;
                 document.getElementById("password").value = data[uid].password;
             }else{
@@ -118,21 +133,24 @@ function password_display(){
         console.error(error);
     });
 }
-
 function no_platform(){
     //display save button
     document.getElementById("save").style.display = "block";
-    console.log("No platform");
-    //set password input value as th generated password
+    //console.log("No platform");
+    //set password input value as the generated password
     document.getElementById("password").value = pd;
     //Set header message
     document.getElementById("log_cred").innerHTML ="Login Credential Not Found";
+    //Save button execution
     document.getElementById("save").addEventListener("click", (e)=>{
+        //prevent default action
         e.preventDefault();
         let messages = [];
+        //identifier input validaton
         if(document.getElementById("identifier").value == ""){
             messages.push("Login Credential required");
         }
+        //password input validaation
         if(document.getElementById("password").value == "" ){
             messages.push("Password is required");
         }
@@ -140,6 +158,7 @@ function no_platform(){
             alert(messages.join("\n"));
         }
         if(document.getElementById("identifier").value != "" && document.getElementById("password").value != ""){
+            //set target to user's available platforms
             const target = ref(database, 'Users/' + user_name + '/' + "Platform/");
             const newPostRef = push(target);
             set(newPostRef, {   //posting data with unique id
@@ -148,29 +167,35 @@ function no_platform(){
                 password: document.getElementById("password").value
             });
             alert("Password Saved");
+            //update the login credential's status
             document.getElementById("log_cred").innerHTML ="Saved Login Credential";
+            //hide save button after saving
             document.getElementById("save").style.display = "none";
             password_display();
         }
     });
 }
-
+//exit button execution
 document.getElementById("exit").addEventListener("click", (e)=>{
     //Prevent default action
     e.preventDefault();
     //Close the current tab
     window.close();
 })
-
+//Password button execution
 document.getElementById("ctc").addEventListener("click", (e)=>{
+    //Prevent default action
     e.preventDefault();
+    //select the password value
     const text = document.getElementById('password');
     text.select();
+    //Copy to clipboard
     navigator.clipboard.writeText(text.value);
 })
 document.getElementById("toggle_password").addEventListener("click", (e)=>{
     //e.preventDefault();
     const password = document.getElementById("master_password");
+    //toggle master password field visibility
     if (password.type === "password") {
         password.type = "text";
         document.getElementById("toggle_password").src = "img/eye-icon-slash.png";
@@ -182,6 +207,7 @@ document.getElementById("toggle_password").addEventListener("click", (e)=>{
 document.getElementById("toggle_password2").addEventListener("click", (e)=>{
     //e.preventDefault();
     const password = document.getElementById("password");
+    //toggle password field visibility
     if (password.type === "password") {
         password.type = "text";
         document.getElementById("toggle_password2").src = "img/eye-icon-slash.png";
